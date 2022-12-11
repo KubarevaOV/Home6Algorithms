26b3adfe54b226c605ec059b7522f37a4a45d7a4
Dmitry Kuklin <dmi-kuklin@yandex.ru>
2022-12-12 02:08:04 +0300

    Home tasks 41, 43

---
+++ Task41/Program.cs
@@ -0,0 +1,18 @@
+﻿Random rnd = new Random();
+int m  = rnd.Next(1, 11);
+int counter = 0;
+
+int[] array = new int[m];
+Console.WriteLine("Введите {0} чисел", (m));
+
+for (int i = 0; i<m; i++) {
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+for (int i = 0; i<array.Length; i++) {
+    if (array[i] > 0) {
+        counter ++;
+    }
+}
+
+Console.WriteLine("Вы ввели {0} чисел больше 0", counter);
---
+++ Task43/Program.cs
@@ -0,0 +1,26 @@
+﻿double x = 0;
+double y = 0;
+
+Console.WriteLine("Введите b1");
+int b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите k1");
+int k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите b2");
+int b2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите k2");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+if ((k1 == k2) && (b1 == b2)) {
+    Console.WriteLine("Прямые совпадают");
+} else if (k1==k2) {
+    Console.WriteLine("Прямые параллельны");
+} else {
+    x = (b2-b1) / (k1-k2);
+
+    y = (k1 * (b2-b1)) / (k1-k2) + b1;
+
+    Console.WriteLine("(x:{0}, y:{1})", x, y);
+}
